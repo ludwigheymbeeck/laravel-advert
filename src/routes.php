5f20974564c7374af04662bb8bf5718e21537bc7
ludwigheymbeeck <ludwigheymbeeck@gmail.com>
2017-03-16 12:43:37 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::get('a/{advert_id}', 'Adumskis\LaravelAdvert\AdvertManagerController@index');
+Route::get('a/{advert_id}', 'LudwigHeymbeeck\LaravelAdvert\AdvertManagerController@index');
